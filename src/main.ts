0c441cf75a66fa1f768de414b15eb4170449b65f
BilalurRehman-27 <bilal_ur_rehman@outlook.com>
2024-05-31 07:14:00 +0500

    chore: Add prettier configuration and customize Swagger UI in main.ts and vercel.json

--- src/main.ts
+++ src/main.ts
@@ -48,11 +48,24 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
+
   SwaggerModule.setup('api', app, document, {
     swaggerOptions: {
       persistAuthorization: true,
     },
+    customSiteTitle: 'NestJs Swagger Documentation',
+    customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
   });
+
   const configService = app.get<ConfigService>(ConfigService);
 
   await app.listen(configService.get<number>('app.port'), () => {
